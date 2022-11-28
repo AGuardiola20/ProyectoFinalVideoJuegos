b576c32e150c6b7a9d1b103868a1c8e1930d0d37
AGuardiola20 <andres.pguardiola@gmail.com>
2022-11-28 09:52:49 -0600

    Efectos de sonido y detalles listos

---
+++ Assets/Script/BossShoot.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShoot : MonoBehaviour
+{
+     private float LastShoot;
+
+    public GameObject Player;
+    public GameObject BulletPrefab;
+
+    private int Health = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si el player esta muerto este codigo no se ejecuta por el return
+        if(Player == null) return;
+        
+        Vector3 direction = Player.transform.position - transform.position;
+        if(direction.x > 0.0f) transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+        else transform.localScale = new Vector3 (-1.0f, 1.0f, 1.0f);
+
+        //Mathf.Abs(x + y) = Conseguir num positivo con el valor absoluto
+        float distance = Mathf.Abs(Player.transform.position.x - transform.position.x);
+
+        if(distance < 0.8f && Time.time > LastShoot + 0.90f){
+            Shoot();
+            LastShoot = Time.time;
+        }
+    }
+
+    private void Shoot(){
+        //Debug.Log("Enemy Shoot");
+        Vector3 direction;
+
+        if(transform.localScale.x == 1.0f) direction = Vector3.right;
+        else direction = Vector3.left;
+
+        GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        bullet.GetComponent<Bullet>().SetDirection(direction);
+    }
+
+    //Hit
+    public void Hit(){
+        Health = Health - 1;
+        if(Health == 0) Destroy(gameObject);
+    }
+}
